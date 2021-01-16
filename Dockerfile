936880ccbfa39361ceb30c4b90e9c9f9b12d705f
李旭 <xulovemin1413@163.com>
2021-01-16 20:49:55 +0800

    修改成docker-compose方式

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,5 @@
 FROM anapsix/alpine-java:8_server-jre_unlimited
-
 WORKDIR /app
-
 EXPOSE 80
-
 ADD ./target/demo-0.1.jar app.jar
-
 CMD java -jar app.jar
--- starup.sh
+++ starup.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 docker build -t demo .
-num=$(docker ps | grep demo | wc -l)
-if [ $num == 1 ]
-then 
-    docker rm -f demo
-fi
-docker run -d --name demo -p 8088:80 demo
+docker-compose down
+docker-compose up -d
